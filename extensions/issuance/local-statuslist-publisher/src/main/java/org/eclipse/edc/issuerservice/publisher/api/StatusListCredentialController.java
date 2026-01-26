ff368592617cc9b2f2538f94685209d5b47649d6
Simeon Manolov [GATE] <simeon.manolov@gate-ai.eu>
2026-01-27 01:26:31 +0200

    GATE README & patches

--- extensions/issuance/local-statuslist-publisher/src/main/java/org/eclipse/edc/issuerservice/publisher/api/StatusListCredentialController.java
+++ extensions/issuance/local-statuslist-publisher/src/main/java/org/eclipse/edc/issuerservice/publisher/api/StatusListCredentialController.java
@@ -65,6 +65,8 @@ public class StatusListCredentialController {
         var split = httpUrl.getPath().split("/");
         var credentialId = split[split.length - 1];
 
+        monitor.info("Checking credentialstatus for '" + credentialId + "'");
+
         var query = QuerySpec.Builder.newInstance()
                 .filter(new Criterion("verifiableCredential.credential.id", "=", credentialId))
                 .filter(new Criterion("metadata.published", "=", true))
@@ -74,6 +76,7 @@ public class StatusListCredentialController {
                 .orElseThrow(InvalidRequestException::new);
 
         if (statusListCredential.isEmpty()) {
+            monitor.debug("No such published credential: '" + credentialId + "'");
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         if (statusListCredential.size() > 1) {
